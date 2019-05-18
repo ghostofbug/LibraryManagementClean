7c80d26cddb7b36e5404584a6c0a578067688e16
le thanh <ghostofbug@live.com>
2019-05-18 14:55:53 +0700

    upload2

--- New folder/LibraryManagement/FormAndCharge.h
+++ New folder/LibraryManagement/FormAndCharge.h
@@ -1,3 +1,21 @@
-#pragma once
+#ifndef __FormAndCharge_H__
+#define __FormAndCharge_H__
+#include "ReaderManage.h"
+#include "BookManage.h"
+#include "PublicFunc.h"
+#include <ctime>
 
+
+struct BorrowForm
+{
+	Reader Person;
+	Book Bookname[50];
+	Date Borrowday;
+	Date Expectreturnday;
+	Time TimeCreate;
+	int BookBorrowed;
+};
+void CreateBorrowForm(list <BorrowForm> &bf, list<Reader> reader);
+void FindReaderInfo(BorrowForm &Borrowbook, list<Reader>l);
+#endif
 
